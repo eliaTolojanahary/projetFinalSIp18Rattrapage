5a30396c22d9a4bb876c8a64f8702187c02ed3dc
Jemima-Harisoa <jemimarazafimandimby@gmail.com>
2026-07-20 12:52:43 +0300

    delete route livre

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,15 +7,6 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'DashboardOperator::index');
 
-
-$routes->get('livres', 'Livres::index');
-$routes->get('livres/(:num)', 'Livres::show/$1');
-$routes->get('livres/ajouter', 'Livres::create');
-$routes->post('livres/ajouter', 'Livres::store');
-$routes->post('livres/(:num)/supprimer', 'Livres::delete/$1');
-$routes->post('livres/(:num)/preter', 'Livres::preter/$1');
-$routes->post('livres/(:num)/retour', 'Livres::retour/$1');
-
 $routes->get('clients', 'ClientsOperator::index');
 $routes->get('clients/(:num)', 'ClientsOperator::show/$1');
 
